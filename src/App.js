e8ba111aac9a2b5badd6d747057fc304cadf2ad7
Ozoperezoso <ricardoosorio097@gmail.com>
2022-08-30 07:50:05 -0500

    unknown push

--- src/App.js
+++ src/App.js
@@ -2,35 +2,76 @@ import React from 'react';
 import './App.css';
 import Testimonio from './components/testimonio';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='main-wrapper'>
-        <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        <Testimonio
-          nombre='Emma Bostian '
-          pais='Sweden'
-          imagen='Emma'
-          cargo='Software Engineer'
-          empresa='Spotify'
-          testimonio={<>I have always struggled with learning JavaScript. I have taken many courses but<strong> freeCodeCamp's course was the one which stuck.</strong>  Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.</>}/>
+
+class App extends React.component{
+  render() {
+    return (
+      <div className="App">
+        <div className='main-wrapper'>
+          <h1>Here is what our alumni say about freeCodeCamp:</h1>
           <Testimonio
-          nombre='Shawn Wang '
-          pais='Singapore'
-          imagen='Shawn'
-          cargo='Software Engineer'
-          empresa='Amazon'
-          testimonio={<>Its scary to change careers. <strong>I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp</strong>. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.</>}/>
-          <Testimonio
-          nombre='Sarah Chima'
-          pais='Nigeria'
-          imagen='Sarah'
-          cargo='Software Engineer'
-          empresa='ChatDesk'
-          testimonio={<React.Fragment><span className='bold'>freeCodeCamp was the gateway to my career as a software developer</span>.The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.</React.Fragment>}/>
+            nombre='Emma Bostian '
+            pais='Sweden'
+            imagen='Emma'
+            cargo='Software Engineer'
+            empresa='Spotify'
+            testimonio={<>I have always struggled with learning JavaScript. I have taken many courses but<strong> freeCodeCamps course was the one which stuck.</strong>  Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.</>}/>
+            
+            <Testimonio
+            nombre='Shawn Wang '
+            pais='Singapore'
+            imagen='Shawn'
+            cargo='Software Engineer'
+            empresa='Amazon'
+            testimonio={<>Its scary to change careers. <strong>I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp</strong>. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.</>}/>
+            
+            <Testimonio
+            nombre='Sarah Chima'
+            pais='Nigeria'
+            imagen='Sarah'
+            cargo='Software Engineer'
+            empresa='ChatDesk'
+            testimonio={<React.Fragment><span className='bold'>freeCodeCamp was the gateway to my career as a software developer</span>.The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.</React.Fragment>}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
+
+/*
+
+Componente funcional
+ function App() {
+   return (
+     <div className="App">
+       <div className='main-wrapper'>
+         <h1>Here is what our alumni say about freeCodeCamp:</h1>
+         <Testimonio
+           nombre='Emma Bostian '
+           pais='Sweden'
+           imagen='Emma'
+           cargo='Software Engineer'
+           empresa='Spotify'
+           testimonio={<>I have always struggled with learning JavaScript. I have taken many courses but<strong> freeCodeCamp's course was the one which stuck.</strong>  Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.</>}/>
+           <Testimonio
+           nombre='Shawn Wang '
+           pais='Singapore'
+           imagen='Shawn'
+           cargo='Software Engineer'
+           empresa='Amazon'
+           testimonio={<>Its scary to change careers. <strong>I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp</strong>. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.</>}/>
+           <Testimonio
+           nombre='Sarah Chima'
+           pais='Nigeria'
+           imagen='Sarah'
+           cargo='Software Engineer'
+           empresa='ChatDesk'
+           testimonio={<React.Fragment><span className='bold'>freeCodeCamp was the gateway to my career as a software developer</span>.The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.</React.Fragment>}/>
+       </div>
+     </div>
+   );
+ }
+
+export default App; */
--- src/components/testimonio.js
+++ src/components/testimonio.js
@@ -1,6 +1,39 @@
+import React from 'react';
 import react from 'react';
 import'../hojas-de-estilo/testimonio.css';
 
+// con componente de clase
+
+class Testimonio extends React.Component{
+  render(){
+    return (
+      <div className='contenedor-testimonio'>
+        <img 
+          className='imagen-testimonio'
+          alt= {`Foto de ${this.props.nombre}`}
+          src={require(`../imagenes/T-${this.props.imagen}.png`)} />
+          <div className='contenedor-texto-testimonio'>
+            <p className='nombre-testimonio'>
+              <strong>{this.props.nombre}</strong> en {this.props.pais}
+              </p>
+            <p className='cargo-testimonio'>
+              {this.props.cargo} en <strong>{this.props.empresa}</strong>
+              </p>
+            <p className='texto-testimonio'>
+              "{this.props.testimonio}"
+              </p>
+          </div>
+      </div>
+    );
+
+  }
+}
+
+export default Testimonio;
+
+// con componente funcional
+
+/*
 function Testimonio(props){
   return (
     <div className='contenedor-testimonio'>
@@ -24,3 +57,4 @@ function Testimonio(props){
 }
 
 export default Testimonio;
+*/
